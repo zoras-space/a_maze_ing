5fbdde2c5a204c81e225703e46222faf7515db9f
Nyasha Tsvuura <ntsvuura@c4a10c10.42berlin.de>
2026-09-04 13:39:05 +0200

    update

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,5 +1,19 @@
+import sys
+
 from parser import parse_config
 
 
-config = parse_config("config.txt")
-print(config)
+def main():
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: python3 a_maze_ing.py config.txt")
+
+    try:
+        config = parse_config(sys.argv[1])
+    except FileNotFoundError:
+        raise ValueError(f"Configuration file not found: {sys.argv[1]}")
+
+    print(config)
+
+
+if __name__ == "__main__":
+    main()
